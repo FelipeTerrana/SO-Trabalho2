dcb4fad24cc5cb843731ed05776389b1fda671fe
FelipeTerrana <felipetc_7@hotmail.com>
2019-07-07 17:06:37 -0300

    Criar myfs.h e myfs.c

---
+++ myfs.c
@@ -0,0 +1,10 @@
+//
+// Created by felipe on 07/07/19.
+//
+
+#include "myfs.h"
+
+int installMyFS()
+{
+    return 0;
+}
---
+++ myfs.h
@@ -0,0 +1,10 @@
+//
+// Created by felipe on 07/07/19.
+//
+
+#ifndef SO_TRABALHO2_MYFS_H
+#define SO_TRABALHO2_MYFS_H
+
+int installMyFS();
+
+#endif //SO_TRABALHO2_MYFS_H
