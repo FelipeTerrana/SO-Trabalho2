566f0605a5b8869a629d3261639ca143e45d6616
FelipeTerrana <felipetc_7@hotmail.com>
2019-07-08 16:27:17 -0300

    Adicionar headers das funções do sistema de arquivos

--- myfs.c
+++ myfs.c
@@ -1,8 +1,40 @@
-//
-// Created by felipe on 07/07/19.
-//
+/*
+*  myfs.c - Implementacao das funcoes relativas ao novo sistema de arquivos
+*
+*  Autores: Eduardo Pereira do Valle -
+*           Felipe Terrana Cazetta - 201635026
+*           Matheus Brinati Altomar -
+*           Vinicius Alberto Alves da Silva -
+*
+*  Projeto: Trabalho Pratico II - Sistemas Operacionais
+*  Organizacao: Universidade Federal de Juiz de Fora
+*  Departamento: Dep. Ciencia da Computacao
+*
+*/
 
 #include "myfs.h"
+#include "vfs.h"
+
+
+
+FSInfo myfsInfo =
+{
+    'm',
+    "myfs",
+    myfsIsIdle,
+    myfsFormat,
+    myfsOpen,
+    myfsRead,
+    myfsWrite,
+    myfsClose,
+    myfsOpendir,
+    myfsReaddir,
+    myfsLink,
+    myfsUnlink,
+    myfsClosedir
+};
+
+
 
 int installMyFS()
 {
--- myfs.h
+++ myfs.h
@@ -1,10 +1,34 @@
-//
-// Created by felipe on 07/07/19.
-//
+/*
+*  myfs.h - Header da API do novo sistema de arquivos
+*
+*  Autores: Eduardo Pereira do Valle -
+*           Felipe Terrana Cazetta - 201635026
+*           Matheus Brinati Altomar -
+*           Vinicius Alberto Alves da Silva -
+*
+*  Projeto: Trabalho Pratico II - Sistemas Operacionais
+*  Organizacao: Universidade Federal de Juiz de Fora
+*  Departamento: Dep. Ciencia da Computacao
+*
+*/
 
 #ifndef SO_TRABALHO2_MYFS_H
 #define SO_TRABALHO2_MYFS_H
 
+#include "disk.h"
+
 int installMyFS();
+
+int myfsIsIdle(Disk *d);
+int myfsFormat(Disk *d, unsigned int blockSize);
+int myfsOpen(Disk *d, const char *path);
+int myfsRead(int fd, char *buf, unsigned int nbytes);
+int myfsWrite(int fd, const char *buf, unsigned int nbytes);
+int myfsClose(int fd);
+int myfsOpendir(Disk *d, const char *path);
+int myfsReaddir(int fd, char *filename, unsigned int *inumber);
+int myfsLink(int fd, const char *filename, unsigned int inumber);
+int myfsUnlink(int fd, const char *filename);
+int myfsClosedir(int fd);
 
 #endif //SO_TRABALHO2_MYFS_H
